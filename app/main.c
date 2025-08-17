43bd6e1bb09976fceb9c04e481829beb05135a48
QAED5552 <a391617248@163.com>
2025-08-17 21:45:41 +0800

    已完成对各延时方法的测试

--- app/main.c
+++ app/main.c
@@ -1,4 +1,10 @@
-#include "main.h"
+#define TIMER_DELAY_MODE 1
+
+
+#include "stm32f10x.h"
+#include "gpio_init_custom.h"
+#include "delay.h"
+
 
 int main(void)
 {
--- app/main.h
+++
@@ -1,3 +0,0 @@
-#include "stm32f10x.h"
-#include "gpio_init_custom.h"
-#include "delay.h"
