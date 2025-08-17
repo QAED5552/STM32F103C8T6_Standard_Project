57d2f94d2805219b29d82475000699468db3c2dc
QAED5552 <a391617248@163.com>
2025-08-17 21:54:59 +0800

    恢复测试用例为默认值，添加注释

--- app/main.c
+++ app/main.c
@@ -1,10 +1,8 @@
-#define TIMER_DELAY_MODE 1
-
-
 #include "stm32f10x.h"
+
+//延时模式切换在#include delay.h之前，否则会有警告（如何定义详参delay.h）
 #include "gpio_init_custom.h"
 #include "delay.h"
-
 
 int main(void)
 {
