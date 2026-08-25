af628fa757f6175c26593e427f3c0ffb0296f04c
QAED5552 <a391617248@163.com>
2026-08-25 19:13:05 +0800

    迁移头文件引用至main.h

--- app/main.c
+++ app/main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "delay.h"
 
 int main(void)
 {
--- app/main.h
+++ app/main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 #include "stm32f10x.h"
+
+#define TIMER_DELAY_MODE 0
 //目前延时模式有三种，但都是阻塞式延时，如需使用非阻塞延时需自行在delay.h中添加.
 //1.普通模式延时(默认模式) #define TIMER_DELAY_MODE 0
 //2.定时器模式延时 #define TIMER_DELAY_MODE 1
 //3.SYSTICK模式延时 #define TIMER_DELAY_MODE 2
-#define TIMER_DELAY_MODE 0
+#include "delay.h"
 
 #endif // MAIN_H
